b3780ec75ed11fda7ab7aa802dbb38bd8f56a9f4
Tim Murphy <tim@26tp.com>
2013-07-04 08:13:37 +1000

    Update assembly info.

--- Projects/BddMagic.Specifications/Properties/AssemblyInfo.cs
+++ Projects/BddMagic.Specifications/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿// todo: Shared common settings with all projects.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,10 +8,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BddMagic.Specifications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BDD Framework for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BddMagic.Specifications")]
+[assembly: AssemblyCompany("Tim Murphy")]
+[assembly: AssemblyProduct("BddMagic")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -22,15 +24,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4119ede8-f5e0-46cd-aae6-95f16687e312")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version number is stamped by MyGet build services.
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- Projects/BddMagic/Properties/AssemblyInfo.cs
+++ Projects/BddMagic/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// todo: configure
+﻿// todo: Shared common settings with all projects.
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BddMagic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BDD Framework for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tim Murphy")]
 [assembly: AssemblyProduct("BddMagic")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -24,15 +24,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1b675dd0-1558-4941-94c2-1f46a2ea3f89")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version number is stamped by MyGet build services.
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
