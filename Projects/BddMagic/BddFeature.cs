fc641dccd2b5857241933168c19a95407d0b32f7
Tim Murphy <tim@26tp.com>
2013-07-03 11:33:11 +1000

    Initial design spike of 'DSL like' syntax.

---
+++ Projects/BddMagic/BddFeature.cs
@@ -0,0 +1,18 @@
+﻿namespace BddMagic
+{
+    public class BddFeature
+    {
+        public BddFeature(string feature, string story)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Feature { get; private set; }
+        public string Story { get; private set; }
+
+        public Scenario Scenario(string scenarioTitle)
+        {
+            return new Scenario(this, scenarioTitle);
+        }
+    }
+}
---
+++ Projects/BddMagic/Scenario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BddMagic.Core.Helpers;
+
+namespace BddMagic
+{
+    public class Scenario : IHideObjectMembers
+    {
+        public Scenario(BddFeature feature, string scenarioTitle)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Func<dynamic, dynamic> this[string step]
+        {
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
