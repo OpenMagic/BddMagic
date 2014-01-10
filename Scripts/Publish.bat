08883d2b73cf81e1826bd9e124fa7f6c9717fc50
Tim Murphy <tim@26tp.com>
2014-01-11 07:33:48 +1100

    New publish script.

---
+++ Scripts/Publish.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo.
+echo Checking out master...
+echo.
+git checkout master
+if not "%errorlevel%" == "0" exit %errorlevel%
+
+echo.
+echo Merging develop into master...
+echo.
+git merge test
+if not "%errorlevel%" == "0" exit %errorlevel%
+
+echo.
+echo Pushing master...
+echo.
+git push
+if not "%errorlevel%" == "0" exit %errorlevel%
+
+echo.
+echo Switching back to develop...
+echo.
+git checkout develop
+exit %errorlevel%
