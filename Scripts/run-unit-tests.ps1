86cf36e657bb4cbf94dfba90b79efeabc4de1efd
Tim Murphy <tim@26tp.com>
2023-11-28 16:28:26 +1100

    Fix run-unit-test.ps1

--- Scripts/run-unit-tests.ps1
+++ Scripts/run-unit-tests.ps1
@@ -1,17 +1,10 @@
 Param (
     [Parameter(Mandatory=$True)]
-    [string]$xunitPath,
-
-    [Parameter(Mandatory=$True)]
     [string]$testsFolder,
 
     [Parameter(Mandatory=$True)]
     [string]$buildConfiguration
     )
-
-if (-Not (Test-Path $xunitPath)) {
-    throw "xunitPath '$xunitPath' does not exist."
-}
 
 if (-Not (Test-Path $testsFolder)) {
     throw "testsPath '$testsFolder' does not exist."
@@ -28,7 +21,7 @@ Get-ChildItem -Path $testsFolder -Directory |
             Write-Host "----------------------------------------------------------------------"
             Write-Host
 
-            Invoke-Expression "&""$xunitPath"" ""$testAssembly"""
+            Invoke-Expression "&mstest ""$testAssembly"""
             
             if ($LASTEXITCODE -ne 0) {
                 throw "One or more unit tests failed in $testAssembly"
